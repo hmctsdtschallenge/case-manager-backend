6ec47568c916424e8137a86ffe6fd246fda99caf
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-09 12:13:27 +0100

    initial commit

---
+++ src/main/java/com/dts/case_manager_backend/CaseManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dts.case_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dts/case_manager_backend/CaseManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dts.case_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

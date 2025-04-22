c14697e0f9dc048655e8efe7b33f2e04f193008d
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-22 11:08:14 +0100

    feat: implement TaskService interface, with abstract methods: createTask, retrieveTaskById, retrieveAllTasks, updateTaskStatus, deleteTask

---
+++ src/main/java/com/dts/case_manager_backend/service/TaskService.java
@@ -0,0 +1,14 @@
+package com.dts.case_manager_backend.service;
+
+import com.dts.case_manager_backend.model.Task;
+import com.dts.case_manager_backend.model.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {
+    Task createTask(TaskDTO taskDTO);
+    Task retrieveTaskById(Long id);
+    List<Task> retrieveAllTasks();
+    Task updateTaskStatus(Long id);
+    void deleteTask(Long id);
+}

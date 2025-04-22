be4a218e438dfb381a9c44a761e5e93198add7d5
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-22 17:04:09 +0100

    feat: implement TaskController with empty methods for API endpoints corresponding to TaskService methods

---
+++ src/main/java/com/dts/case_manager_backend/controller/TaskController.java
@@ -0,0 +1,43 @@
+package com.dts.case_manager_backend.controller;
+
+import com.dts.case_manager_backend.model.Task;
+import com.dts.case_manager_backend.model.TaskDTO;
+import com.dts.case_manager_backend.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> postTask(@RequestBody TaskDTO taskDTO) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> putTaskStatus(@PathVariable long id, @RequestBody String status) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteTask(@PathVariable long id) {
+        return HttpStatus.NO_CONTENT;
+    }
+}

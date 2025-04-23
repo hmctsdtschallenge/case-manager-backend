eeb8b531d07b31e53103e7438ff5a9202ed6600f
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-23 15:30:22 +0100

    feat: implement GlobalExceptionHandler class, with ControllerAdvice annotation, and with methods for handling TaskNotFoundException, and InvalidDTOException

---
+++ src/main/java/com/dts/case_manager_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.dts.case_manager_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleAlbumNotFoundException(TaskNotFoundException e){
+
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+
+        ErrorObject errorObject = new ErrorObject(httpStatus.value(),e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<>(errorObject, httpStatus);
+    }
+
+    @ExceptionHandler(InvalidDTOException.class)
+    public ResponseEntity<ErrorObject> handleInvalidDTOException(InvalidDTOException e){
+
+        HttpStatus httpStatus = HttpStatus.NOT_ACCEPTABLE;
+
+        ErrorObject errorObject = new ErrorObject(httpStatus.value(),e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<>(errorObject, httpStatus);
+    }
+}
